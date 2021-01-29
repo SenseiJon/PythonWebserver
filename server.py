c7e131796537d4858061032851cbdaa0e229f3b2
Jonathan Bethune (TRI-AD)/ジョナサン　ベスーン <10375@MacBook-Pro.local>
2021-01-29 14:39:29 +0900

    small change

--- server.py
+++ server.py
@@ -6,5 +6,5 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Instructor Jon's cool server is always running at port:", PORT)
+    print("Instructor Jon's amazing server is now running at port:", PORT)
     httpd.serve_forever()
